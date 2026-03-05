4187c34646dd49e1e808488c1fc42909d31937bf
Peter Haring <lharing@mun.ca>
2026-03-05 15:21:23 -0330

    Updated website contents and projects using Cursor

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+// Mobile navigation toggle
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".site-nav");
+
+if (navToggle && nav) {
+  navToggle.addEventListener("click", () => {
+    nav.classList.toggle("open");
+  });
+}
+
 
